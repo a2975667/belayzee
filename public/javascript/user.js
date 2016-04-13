d79cd979475d9630d1a62169bdf5622e7d5dced4
tcheng <tcheng@link.cuhk.edu.hk>
2016-04-13 18:35:10 +0800

    UI enhance, feature polish

--- public/javascript/user.js
+++ public/javascript/user.js
@@ -18,7 +18,14 @@ $(document).ready(function() {
             console.log("hihi");
             var uid = $("#ok").attr("value");
             console.log(uid);
-            var uname = $("#DisN").html();
+            var uname = $("#DisN").html().trim();
+            if(uname == ""){
+              window.alert("Cannot be blank!");
+              setTimeout(function() {
+                  window.location = "/profile";
+              }, 1000);
+              return;
+            }
             var msg = "change display name to " + uname + " ?";
             if(confirm(msg)){
               var data = {};
